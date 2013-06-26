d466505987a17c76c1a1429a4a4fa7fc31085a25
Mihhail Arhipov <mihhail.arhipov@gmail.com>
2013-06-26 15:54:46 +0100

    Module initial files

---
+++ js/stickynav.js
@@ -0,0 +1,22 @@
+(function($) {
+  Drupal.behaviors.stickynav = {
+    attach : function(context) {
+      var selector = Drupal.settings.stickynav.selector;
+      //only getting the first elmenet in the dom
+      var menu = $(selector).eq(0);
+      var breakpoint = menu.offset().top;
+      //we need to compensate the element so that the content does not jump up
+      var compensation = menu.outerHeight();
+      //attaching a scroll event
+      $(window).scroll(function () {
+        if ($(window).scrollTop() > breakpoint) {
+          menu.addClass('stickynav-active');
+          $('body').css('padding-top', compensation);
+        } else {
+          menu.removeClass('stickynav-active');
+          $('body').css('padding-top', 'inherit');
+        }
+      });
+    }
+  }
+})(jQuery);
