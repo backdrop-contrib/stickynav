272fe4fc2051b61f050ed637fa5732ebcba6dc9a
acke <acke@765764.no-reply.drupal.org>
2013-10-09 19:58:04 +0100

    Issue #2102901 by acke: fixed event attaching even when element is not present

--- js/stickynav.js
+++ js/stickynav.js
@@ -4,19 +4,22 @@
       var selector = Drupal.settings.stickynav.selector;
       //only getting the first elmenet in the dom
       var menu = $(selector).eq(0);
-      var breakpoint = menu.offset().top;
-      //we need to compensate the element so that the content does not jump up
-      var compensation = menu.outerHeight();
-      //attaching a scroll event
-      $(window).scroll(function () {
-        if ($(window).scrollTop() > breakpoint) {
-          menu.addClass('stickynav-active');
-          $('body').css('padding-top', compensation);
-        } else {
-          menu.removeClass('stickynav-active');
-          $('body').css('padding-top', 'inherit');
-        }
-      });
+      if (menu.length) {
+        var breakpoint = menu.offset().top;
+        //we need to compensate the element so that the content does not jump up
+        var compensation = menu.outerHeight();
+        //attaching a scroll event
+        $(window).scroll(function () {
+          console.log(1);
+          if ($(window).scrollTop() > breakpoint) {
+            menu.addClass('stickynav-active');
+            $('body').css('padding-top', compensation);
+          } else {
+            menu.removeClass('stickynav-active');
+            $('body').css('padding-top', 'inherit');
+          }
+        });
+      }
     }
   }
 })(jQuery);
