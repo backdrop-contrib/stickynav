65fc6185039f50488b9d2f3bfe82d16e7dd9ab49
Mihhail Arhipov <mihhail.arhipov@gmail.com>
2014-06-09 21:20:23 +0100

    Issue #2282775 by msemenov: fixed breakpoint changing on behavior call.

--- js/stickynav.js
+++ js/stickynav.js
@@ -4,8 +4,12 @@
       var selector = Drupal.settings.stickynav.selector;
       //only getting the first elmenet in the dom
       var menu = $(selector).eq(0);
+      var breakpoint = null;
       if (menu.length) {
-        var breakpoint = menu.offset().top;
+        //only set breakpoint once so that it doesn't reset in a behavior attach happens.
+        if (breakpoint) {
+          breakpoint = menu.offset().top;
+        }
         //we need to compensate the element so that the content does not jump up
         var compensation = menu.outerHeight();
         //attaching a scroll event
