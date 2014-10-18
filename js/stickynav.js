60b9d6c14e9976451169d9a5aca1a1ad3c8e7f12
Mihhail Arhipov <mihhail.arhipov@haymarket.com>
2014-10-18 19:47:16 +0100

    Refactored js to use once plugin

--- js/stickynav.js
+++ js/stickynav.js
@@ -1,26 +1,31 @@
 (function($) {
   Drupal.behaviors.stickynav = {
+    breakpoint: 0,
+    compensation: 0,
+    originalPadding: 0,
     attach : function(context) {
       var selector = Drupal.settings.stickynav.selector;
       //only getting the first elmenet in the dom
-      var menu = $(selector).eq(0);
-      var breakpoint = null;
-      if (menu.length) {
-        //only set breakpoint once so that it doesn't reset in a behavior attach happens.
-        if (breakpoint === null) {
-          breakpoint = menu.offset().top;
-        }
-        //we need to compensate the element so that the content does not jump up
-        var compensation = menu.outerHeight();
-        //attaching a scroll event
-        $(window).scroll(function () {
-          if ($(window).scrollTop() > breakpoint) {
-            menu.addClass('stickynav-active');
-            $('body').css('padding-top', compensation);
-          } else {
-            menu.removeClass('stickynav-active');
-            $('body').css('padding-top', 'inherit');
-          }
+      var $menu = $(selector).eq(0);
+      if ($menu.length) {
+        $menu.once('stickynav', function() {
+          // Save original padding on top. 0 timeout to get correct padding.
+          setTimeout(function() {
+            Drupal.behaviors.stickynav.originalPadding = $('body').css('paddingTop');
+          }, 0);
+          Drupal.behaviors.stickynav.breakpoint = $menu.offset().top;
+          //we need to compensate the element so that the content does not jump up.
+          Drupal.behaviors.stickynav.compensation = $menu.outerHeight();
+          $(window).scroll(function () {
+            if ($(window).scrollTop() > Drupal.behaviors.stickynav.breakpoint) {
+              $menu.addClass('stickynav-active');
+              $('body').css('padding-top', Drupal.behaviors.stickynav.compensation);
+            }
+            else {
+              $menu.removeClass('stickynav-active');
+              $('body').css('padding-top', Drupal.behaviors.stickynav.originalPadding);
+            }
+          });
         });
       }
     }
