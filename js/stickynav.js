9acd07bbae5f7d594c011bfa73c56dd7b78cd7a9
Mihhail Arhipov <mihhail.arhipov@gmail.com>
2015-01-14 10:14:35 +0000

    Issue #2404813 by klonos: Allow setting a top offset

--- js/stickynav.js
+++ js/stickynav.js
@@ -7,22 +7,31 @@
       var selector = Drupal.settings.stickynav.selector;
       //only getting the first elmenet in the dom
       var $menu = $(selector).eq(0);
+      var offset = 0;
       if ($menu.length) {
         $menu.once('stickynav', function() {
           // Save original padding on top. 0 timeout to get correct padding.
           setTimeout(function() {
             Drupal.behaviors.stickynav.originalPadding = $('body').css('paddingTop');
           }, 0);
-          Drupal.behaviors.stickynav.breakpoint = $menu.offset().top;
+          offset += parseInt(Drupal.settings.stickynav.offsets.custom_offset);
+          $(Drupal.settings.stickynav.offsets.selector).each(function() {
+            offset += $(this).outerHeight();
+          });
+          Drupal.behaviors.stickynav.breakpoint = $menu.offset().top - offset;
           //we need to compensate the element so that the content does not jump up.
           Drupal.behaviors.stickynav.compensation = $menu.outerHeight();
           $(window).scroll(function () {
             if ($(window).scrollTop() > Drupal.behaviors.stickynav.breakpoint) {
               $menu.addClass('stickynav-active');
+              if (offset) {
+                $menu.css({'top': offset + 'px'});
+              }
               $('body').css('padding-top', Drupal.behaviors.stickynav.compensation);
             }
             else {
               $menu.removeClass('stickynav-active');
+              $menu.css({'top': ''});
               $('body').css('padding-top', Drupal.behaviors.stickynav.originalPadding);
             }
           });
